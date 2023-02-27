c3aaf2475b6063ec3a68503a3401603ea6ee1929
alexbotha <alexbotha@hotmail.co.uk>
2023-02-27 11:09:27 +0000

    commit tree 0287018c3527f1c6a07fad42e2c7ed485d053df5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,33 @@
 require 'pry'
-
-class String
+  
+class String 
 
   def sentence?
-    
-  end
+    self.end_with?(".")
+  end 
 
   def question?
-
-  end
+    self.end_with?("?")
+  end 
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    self.split.count
+  end 
 
-  end
-end
+  def count_sentences
+    x = self.split(/[!?.]+/)
+   
+    x.length
+end 
+
+end 
+
+# let sentence = 0 
+# arr = [!,?,.]
+#sentence.each.include(arr)
+#sentence += 1
+
